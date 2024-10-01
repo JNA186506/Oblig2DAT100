2971f0a7b4dea9bacaedffd336e7a940824dab11
JohannesNA <186506@stud.hvl.no>
2024-10-01 12:48:43 +0200

    Oppdaterte Oppgaver JNA

--- src/main/java/no/hvl/dat100/matriser/Matriser.java
+++ src/main/java/no/hvl/dat100/matriser/Matriser.java
@@ -2,11 +2,6 @@ package no.hvl.dat100.matriser;
 
 public class Matriser {
 
-	public static void main(String[] args) {
-		int[][] m = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-		skrivUt(m);
-	}
-
 	// a)
 	// Implementer en metode som kan skrive ut en matrise.
 	public static void skrivUt(int[][] matrise) {
@@ -32,30 +27,55 @@ public class Matriser {
 	// (fler-dimensjonell tabell) som er like stor som parameteren og så
 	// multiplisere alle elementer med tall.
 	public static int[][] skaler(int tall, int[][] matrise) {
-		
+		int [][] skaler = new int[matrise.length][];
+		for (int i = 0; i < matrise.length; i++) {
+			skaler[i] = new int[matrise[i].length];
+			for (int j = 0; j < matrise[i].length; j++) {
+				skaler[i][j] = matrise[i][j] * tall;
+			}
+		}
+		return skaler;
 	}
 
 	// d)
 	// Implementer en metode som avgjør om to matriser gitt ved parametrene a og b
 	// er like.
 	public static boolean erLik(int[][] mat1, int[][] mat2) {
-		
+		// TODO
+		throw new UnsupportedOperationException("Metoden ikke implementert");
 	}
 
 	// e)
 	public static int[][] speile(int[][] matrise) {
+		int[][] speile = new int[matrise.length][];
 
-		// TODO
-
-		throw new UnsupportedOperationException("Metoden speile ikke implementert");
+		for (int i = 0; i < matrise.length; i++) {
+			speile[i] = new int [matrise.length];
+			for (int j = 0; j < matrise[i].length; j++) {
+				speile[i][j] = matrise[j][i];
+			}
+		}
+		return speile;
 
 	}
 
 	// f)
 	public static int[][] multipliser(int[][] a, int[][] b) {
+		int[][] multipliser = new int[a.length][];
+		// i = rader
+		// j = kolonner
+		// k = ganger sammen rad0 kol0, rad1 kol1 osv.
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden multipliser ikke implementert");
+		for (int i = 0; i < a.length; i++) {
+			multipliser[i] = new int[a[i].length];
+			for (int j = 0; j < a[i].length; j++) {
+				for (int k = 0; k < a[i].length; k++) {
+					multipliser[i][j] += a[i][k] * b[k][j]; // Multipliser[0][0] = a[0][0] * b[0][0]
+					// Multipliser[produkt][produkt] + a[1][1] * b[1][1] osv.
+				}
+			}
+		}
+		return multipliser;
 
 	}
 }
--- src/main/java/no/hvl/dat100/tabeller/Tabeller.java
+++ src/main/java/no/hvl/dat100/tabeller/Tabeller.java
@@ -43,7 +43,12 @@ public class Tabeller {
 
 	// e)
 	public static int posisjonTall(int[] tabell, int tall) {
-		throw new UnsupportedOperationException("Metoden er ikke implementert");
+		for (int i = 0; i < tabell.length; i++) {
+			if (tabell[i] == tall) {
+				return i;
+			}
+		}
+		return -1;
 	}
 
 	// f)
